93575f2e14e8c5653b2c4d34ed08abf35baedbdc
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2023-05-14 01:20:54 -0400

    pygll: organize tests

--- pygll/tests/parser_test.py
+++ pygll/tests/parser_test.py
@@ -2,7 +2,12 @@ import sys
 from ..grammar import *
 from ..parser import *
 
-def test_parser():
+def test_grammar_format():
+    r1 = Rule(NonTerm("A"), (Term("P"), NonTerm("R"), Term("Q")))
+    r2 = Rule(NonTerm("A"), (NonTerm("P"), NonTerm("Q")))
+    print(r1)
+    print(r2)
+
     g = Grammar(NonTerm("A"),
                { NonTerm("A") : { (Term("P")   ,) },
                  NonTerm("B") : { (NonTerm("Q"),) },
@@ -11,10 +16,15 @@ def test_parser():
                })
     print(g)
 
-    r1 = Rule(NonTerm("A"), (Term("P"), NonTerm("R"), Term("Q")))
-    r2 = Rule(NonTerm("A"), (NonTerm("P"), NonTerm("Q")))
-    print(r1)
-    print(r2)
+    g_s = Grammar(NonTerm("S"),
+                  { NonTerm("S") : { (NonTerm("S"), NonTerm("S")),
+                                     (),
+                                     (NonTerm("A"), NonTerm("B")),
+                                     (Term("a"),) },
+                    NonTerm("A") : { (Term("a"), NonTerm("A")) },
+                    NonTerm("B") : { (Term("b"),) }
+                  })
+    print(g_s)
 
     print(GrammarSlot(r1,0))
     print(GrammarSlot(r1,1))
@@ -30,48 +40,47 @@ def test_parser():
     except ValueError:
         pass
 
-    p = productive(g)
-    print(p)
+def test_grammar_build():
+    g = Grammar(NonTerm("A"),
+               { NonTerm("A") : { (Term("P")   ,) },
+                 NonTerm("B") : { (NonTerm("Q"),) },
+                 NonTerm("C") : { (Term("R"),),
+                                  (NonTerm("B"),) }
+               })
 
-    g_1 = shrink(g, p)
-    print(g_1)
-
-    r = reachable(g_1)
-    print(r)
-
-    g_2 = shrink(g_1, r)
-    print(g_2)
-
-    f_1 = buildFirst(g_2)
-    print(f_1)
-
+    p     = productive(g)
+    g_1   = shrink(g, p)
+    r     = reachable(g_1)
+    g_2   = shrink(g_1, r)
+    f_1   = buildFirst(g_2)
     flw_1 = buildFollow(g_2, f_1)
+    print(g)
+    print(p)
+    print(g_1)
+    print(r)
+    print(g_2)
+    print(f_1)
     print(flw_1)
 
-    g_s = Grammar(NonTerm("S"),
-                  { NonTerm("S") : { (NonTerm("S"), NonTerm("S")),
-                                     (),
-                                     (NonTerm("A"), NonTerm("B")),
-                                     (Term("a"),) },
-                    NonTerm("A") : { (Term("a"), NonTerm("A")) },
-                    NonTerm("B") : { (Term("b"),) }
-                  })
-    print(g_s)
+    g = Grammar(NonTerm("S"),
+               { NonTerm("S") : { (NonTerm("S"), NonTerm("S")),
+                                  (),
+                                  (NonTerm("A"), NonTerm("B")),
+                                  (Term("a"),) },
+                 NonTerm("A") : { (Term("a"), NonTerm("A")) },
+                 NonTerm("B") : { (Term("b"),) }
+               })
 
-    ts = productive(g_s)
-    print(ts)
-    g_s = shrink(g_s, ts)
-    print(g_s)
-
-    ts = reachable(g_s)
-    print(ts)
-    g_s = shrink(g_s, ts)
-    print(g_s)
-
-    print(nullable(g_s))
-
-    f_2 = buildFirst(g_s)
-    print(f_2)
-
-    flw_2 = buildFollow(g_s, f_2)
-    print(flw_2)
+    p     = productive(g)
+    g_1   = shrink(g, p)
+    r     = reachable(g_1)
+    g_2   = shrink(g_1, r)
+    f_1   = buildFirst(g_2)
+    flw_1 = buildFollow(g_2, f_1)
+    print(g)
+    print(p)
+    print(g_1)
+    print(r)
+    print(g_2)
+    print(f_1)
+    print(flw_1)
